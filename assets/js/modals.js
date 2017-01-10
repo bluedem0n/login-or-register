23902420460786f701b806cdeee9939c22d0ca9e
Sthefany <sthefanyflorianog@gmail.com>
2017-01-10 10:22:30 -0500

    Login or register

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -1,13 +1,5 @@
 $(function () {
-
 	//LOGIN OR REGISTER VOLUNTEER
-	$('#login-form-link').click(function (e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
 	$('#register-form-link').click(function (e) {
 		$("#register-form").delay(100).fadeIn(100);
 		$("#login-form").fadeOut(100);
@@ -15,6 +7,14 @@ $(function () {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+	$('#login-form-link').click(function (e) {
+		$("#login-form").delay(100).fadeIn(100);
+		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
 	//LOGIN OR REGISTER ORG
 	$('#login-form-org-link').click(function (e) {
 		$("#login-form-org").delay(100).fadeIn(100);
@@ -30,7 +30,6 @@ $(function () {
 		$(this).addClass('active');
 		e.preventDefault();
 	});
-	
 	//MODALS CLOSE
 	$('#volunteer').click(function (e) {
 		$('#elegir').modal('hide');
@@ -38,5 +37,7 @@ $(function () {
 	$('#organization').click(function (e) {
 		$('#elegir').modal('hide');
 	})
-
+	$('#forgot-link').click(function (e) {
+		$('#btn-login').modal('hide');
+	})
 });
